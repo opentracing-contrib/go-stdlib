b6988a9a5fb6be68a7c731323049edd500690cb4
Luca Comellini <luca.com@gmail.com>
2024-11-07 20:44:26 -0800

    Add golangci-lint

--- nethttp/metrics-tracker.go
+++ nethttp/metrics-tracker.go
@@ -1,3 +1,4 @@
+//go:build go1.8
 // +build go1.8
 
 package nethttp
@@ -31,7 +32,7 @@ func (w *metricsTracker) Write(b []byte) (int, error) {
 func (w *metricsTracker) wrappedResponseWriter() http.ResponseWriter {
 	var (
 		hj, i0 = w.ResponseWriter.(http.Hijacker)
-		cn, i1 = w.ResponseWriter.(http.CloseNotifier)
+		cn, i1 = w.ResponseWriter.(http.CloseNotifier) //nolint:staticcheck // TODO: Replace deprecated CloseNotifier
 		pu, i2 = w.ResponseWriter.(http.Pusher)
 		fl, i3 = w.ResponseWriter.(http.Flusher)
 		rf, i4 = w.ResponseWriter.(io.ReaderFrom)
